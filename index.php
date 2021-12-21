d1be36954100b842229156af6f07f9ffd41b7c2e
rofifaghna <85244975+rofifaghna@users.noreply.github.com>
2021-12-21 23:05:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Tugas Pemrograman Web</title>
+  </head>
+  <body>
+  
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a class="navbar-brand" href="index.php"><svg width="20" height="22" fill="#dc3545" class="bi bi-door-open-fill" viewBox="0 0 16 16">
+  <path d="M1.5 15a.5.5 0 0 0 0 1h13a.5.5 0 0 0 0-1H13V2.5A1.5 1.5 0 0 0 11.5 1H11V.5a.5.5 0 0 0-.57-.495l-7 1A.5.5 0 0 0 3 1.5V15H1.5zM11 2h.5a.5.5 0 0 1 .5.5V15h-1V2zm-2.5 8c-.276 0-.5-.448-.5-1s.224-1 .5-1 .5.448.5 1-.224 1-.5 1z"/>
+</svg> Portofolio Perusahaan</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="wpu-movie/index.html">List</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="input.php">Input</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Tabel Portofolio
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="alumni/alumni.php">Alumni</a></li>
+            <li><a class="dropdown-item" href="app/application.php">Application</a></li>
+            <li><a class="dropdown-item" href="perusahaan/perusahaan.php">Perusahaan</a></li>
+            <li><a class="dropdown-item" href="PL/PL.php">Perusahaan Lowongan</a></li>
+            <li><a class="dropdown-item" href="PLS/PLS.php">Perusahaan Lowongan Syarat</a></li>
+            <li><a class="dropdown-item" href="syarat/syarat.php">Syarat</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+  </nav>
+    <!-- akhir navbar -->
+</br>
+
+    <!-- jumbotron -->
+    <div class="container">
+    <section class="jumbotron text-center">
+      <img src="images/—Pngtree—movie clapper_5493441.png" class="card-img-top" alt="Fotoku"  style="width: 20rem;">
+      <h1 class="display-4">Movie Association Company</h1>
+      <p class="lead">Employee Information Center Movie Association Company</p>
+      <hr class="my-4">
+      <p>“I WOULD TRAVEL DOWN TO HELL AND WRESTLE A FILM AWAY FROM THE DEVIL IF IT WAS NECESSARY.”</p>
+    </section>
+    </div>
+    <!-- akhir jumbotron -->
+
+    <a class="btn btn-primary" href="rpl/camera2.html" role="button">Camera</a>
+
+    <a class="btn btn-primary" href="rpl/location.php" role="button">Location</a>
+
+    <a class="btn btn-primary" href="rpl/bluetooth/bluetooth.html" role="button">Bluetooth</a>
+
+    <a class="btn btn-primary" href="rpl/audio.html" role="button">Audio</a>
+
+    <a class="btn btn-primary" href="rpl/notif.html" role="button">Notification</a>
+
+    <a class="btn btn-primary" href="rpl/mic.html" role="button">Microphone</a>
+
+    
+
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
